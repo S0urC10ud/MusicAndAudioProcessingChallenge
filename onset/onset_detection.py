6b98fa62cdf3e3e4aca8ff6c4cdfe7bc853c69dc
danthe1st <daniel@wwwmaster.at>
2024-05-11 20:32:15 +0200

    algorithmic onset detection improvements

--- onset/onset_detection.py
+++ onset/onset_detection.py
@@ -14,7 +14,6 @@ def spectral_difference(spect: npt.NDArray, norm_index: int = 1) -> npt.NDArray:
     positive_diff = np.abs(np.maximum(diff, 0))
     return np.sum(positive_diff**norm_index, axis=1)**(1/norm_index)
 
-
 def onset_detection_function(sample_rate: int, signal: npt.NDArray, fps: int, spect: npt.NDArray, magspect: npt.NDArray,
                              melspect: npt.NDArray, options):
     """
@@ -24,42 +23,71 @@ def onset_detection_function(sample_rate: int, signal: npt.NDArray, fps: int, sp
     """
 
     values = spectral_difference(spect)
-    #plt.plot(values)
-    #plt.title("spectral difference")
-    #plt.show()
-    #values_per_second = sample_rate * spect.shape[1]/len(signal)
+
+    n_outliers = len(values)//25 # //25 means 50 elements considered to be outliers in min/max calculation
+
+    min_value = np.max(values[np.argpartition(values, n_outliers)[:n_outliers]])
+    max_value = np.min(values[np.argpartition(values, -n_outliers)[-n_outliers:]])
+
+    values = 1000*(values-min_value)/(max_value-min_value)
     return values, fps
 
 def adaptive_thresholding(odf: npt.NDArray, adaptive_threshold_window_size:int, required_max_window_size:int, delta:float|int, l:float|int):
     onsets = np.zeros_like(odf, dtype=bool)
     
+    last_picked = -1000
+
     for i in range(len(odf)):
-        low = max(0, i-adaptive_threshold_window_size)
-        high = min(len(odf), i+adaptive_threshold_window_size)
 
-        req_max_low = max(0, i-required_max_window_size)
-        req_max_high = min(len(odf), i+required_max_window_size)
-        is_local_max = odf[i]>=np.max(odf[req_max_low:req_max_high])
+        # if there are multiple possible peaks near each other, peak the first one
+        # this is done by looking at fewer values "in the future" for local max checking
+        # and then skipping a few iterations i.e. not considering peaks after that
 
-        med = np.median(odf[low:high])
-        threshold = delta + l*med
-        
-        onsets[i] = odf[i] > threshold and is_local_max
+        if i > last_picked + required_max_window_size: # avoid multiple peaks
+            low = max(0, i-adaptive_threshold_window_size)
+            high = min(len(odf), i+adaptive_threshold_window_size)
+
+            # require to be the maximum in a local window
+            req_max_low = max(0, i-required_max_window_size)
+            req_max_high = min(len(odf), i+required_max_window_size//3)
+            is_local_max = odf[i]>=np.max(odf[req_max_low:req_max_high])
+
+            med = np.median(odf[low:high])
+            threshold = delta + l*med
+            
+            is_onset = odf[i] > threshold and is_local_max
+            if is_onset:
+                last_picked = i
+            onsets[i] = is_onset
 
     # no onset at very beginning 
     onsets[:2] = False
     return onsets
+
+restore_plot_false = [False]
 
 def detect_onsets(odf_rate: int, odf: npt.NDArray, options):
     """
     Detect onsets in the onset detection function.
     Returns the positions in seconds.
     """
-    # we only have a dumb dummy implementation here.
-    # it returns the timestamps of the 100 strongest values.
-    # this is not a useful solution at all, just a placeholder.
-    
-    onsets = adaptive_thresholding(odf, odf_rate//10, 5, 15, 1.05)
 
-    strongest_indices = np.where(onsets)[0]#fixed threshold
+    # new
+    onsets = adaptive_thresholding(odf,
+        adaptive_threshold_window_size=odf_rate//10, required_max_window_size=odf_rate//15,
+        delta=20, l=1.1)
+
+    strongest_indices = np.where(onsets)[0]
+
+    # Debugging: show plot in case of few detected onsets
+    if restore_plot_false[0]:
+        restore_plot_false[0] = False
+        options.plot = False
+    if len(strongest_indices) <=10:
+        options.plot = True
+        restore_plot_false[0] = True
+        
+        # can add artificial onsets in order to display plot
+        #strongest_indices = np.concatenate([[0,1,2,3], strongest_indices])
+
     return strongest_indices / odf_rate
