d4916b91948ebcf108be683fcf6d03522e8f352f
Lukas Gandler <lukas.gandler@outlook.com>
2024-05-27 16:11:58 +0200

    Tried to improve upon autocorrelation by taking all high peaks belonging to bpm repetitions and aligning them with onsets

--- tempo/autocorrelation.py
+++ tempo/autocorrelation.py
@@ -24,19 +24,6 @@ def find_highest_peak(signal, threshold=0):
     highest_peak_lag_value = np.where(signal == signal[peaks[highest_peak]])[0][0]
     return highest_peak_lag_value
 
-
-def compute_bpm_estimate(peaks, autocorr, fps, min_lag):
-    # NOTE: there are cases where the odf has a hard time detecting onsets resulting in no peaks from the autocorr
-    # Hot-Fix: just return a default BPM
-    if len(peaks) == 0:
-        return (MAX_BPM + MIN_BPM) / 2
-
-    highest_peak = np.argmax(autocorr[peaks])
-    highest_peak_lag = np.where(autocorr == autocorr[peaks[highest_peak]])[0][0]    # sadly not so pretty
-
-    estimated_bpm = 60 / (highest_peak_lag + min_lag) * fps
-    return estimated_bpm
-
 def detect_tempo(sample_rate, signal, fps, spect, magspect, melspect, odf_rate, odf, onsets, options):
     min_lag, max_lag = get_lag_range(fps)
 
